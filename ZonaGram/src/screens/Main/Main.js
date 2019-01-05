4fb19a6df5f5948963fabc044ad6efdecfb3eeba
Victor Lopes <victor.lopesms@gmail.com>
2019-01-05 17:50:26 -0300

    Topbar trouble with slow scroll animation was fixed.

--- ZonaGram/src/screens/Main/Main.js
+++ ZonaGram/src/screens/Main/Main.js
@@ -5,11 +5,14 @@ import {
     View,
     Image,
     ScrollView,
-    Animated
+    Animated,
+    Dimensions
 } from 'react-native';
 
 import {Actions} from 'react-native-router-flux';
 import ProfileItem from './ProfileItem';
+
+var width = Dimensions.get('window').width;
 
 export default class Main extends Component {
 
@@ -28,12 +31,14 @@ export default class Main extends Component {
     });
     return (
         <View style={styles.containter}>
-            <Animated.View style={[styles.topbar, {transform: [{translateY: headMov }]}]}>
-
-            </Animated.View>
             <Animated.ScrollView 
-                style={[styles.profiles, {transform: [{translateY: headMov }]}]}
+                style={styles.profiles}
+                contentContainerStyle={{
+                    paddingTop: 250,
+                    paddingBottom: 25
+                }}
                 onScroll={Animated.event([{nativeEvent: {contentOffset: {y: this.state.scrollY}}}],{useNativeDriver: true})}
+                
                 >
                 <ProfileItem onPress={() => {Actions.chat()}} type='GROUP' title='Grupo 1' lastMessages={[['José', 'Mensagem 1'], ['João', 'Mensagem 2'], ['Maria', 'Mensagem 3'], ['Pedro', 'Mensagem 4 será descartada pelo algoritmo.']]}/>
                 <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 1' />
@@ -47,7 +52,22 @@ export default class Main extends Component {
                 <ProfileItem type='ANONYMOUS' title='Usuário Anônimo #1' />
                 <ProfileItem type='USER' title='Usuário 2'/>
                 <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 2'/>
+                <ProfileItem onPress={() => {Actions.chat()}} type='GROUP' title='Grupo 1' lastMessages={[['José', 'Mensagem 1'], ['João', 'Mensagem 2'], ['Maria', 'Mensagem 3'], ['Pedro', 'Mensagem 4 será descartada pelo algoritmo.']]}/>
+                <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 1' />
+                <ProfileItem type='USER' title='Usuário 1'/>
+                <ProfileItem type='ANONYMOUS' title='Usuário Anônimo #1' />
+                <ProfileItem type='USER' title='Usuário 2'/>
+                <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 2'/>
+                <ProfileItem onPress={() => {Actions.chat()}} type='GROUP' title='Grupo 1' lastMessages={[['José', 'Mensagem 1'], ['João', 'Mensagem 2'], ['Maria', 'Mensagem 3'], ['Pedro', 'Mensagem 4 será descartada pelo algoritmo.']]}/>
+                <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 1' />
+                <ProfileItem type='USER' title='Usuário 1'/>
+                <ProfileItem type='ANONYMOUS' title='Usuário Anônimo #1' />
+                <ProfileItem type='USER' title='Usuário 2'/>
+                <ProfileItem type='HOTGROUP' title='Grupo Dinâmico 2'/>
             </Animated.ScrollView>
+            <Animated.View style={[styles.topbar, {transform: [{translateY: headMov }]}]}>
+                
+            </Animated.View>
         </View>
     );
   }
@@ -62,8 +82,10 @@ const styles = StyleSheet.create({
         padding: 20
     },
     topbar: {
-        height: 250,
+        position: 'absolute',
+        top: 0,
         backgroundColor: 'yellow',
-        width: window.width,
+        width: width,
+        height: 250
     }
 });
